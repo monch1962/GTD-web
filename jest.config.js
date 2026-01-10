8ee447eabfb86f813f423d54aa5b4b1d33ddca8b
David Mitchell <monch1962@gmail.com>
2026-01-10 22:22:29 +1100

    config: Exclude Playwright E2E tests from Jest runs
    - Add testPathIgnorePatterns to jest.config.js to exclude:
      - /tests-e2e/ directory
      - /tests/journeys/ directory
      - /playwright-report/ directory
      - /test-results/ directory
    
    - Move misplaced project-count-regression.test.js from __tests__/ to tests-e2e/
      and rename to .spec.js to match Playwright conventions
    
    This ensures Jest only runs unit tests (1,123 passing) and Playwright tests
    are run separately with 'npm run test:e2e' command.

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,32 @@
 export default {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-  collectCoverageFrom: [
-    'js/**/*.js',
-    '!js/app.js',  // Skip app.js as it's mostly DOM manipulation
-    '!**/node_modules/**'
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
-    }
-  },
-  verbose: true,
-  testTimeout: 10000,
-  transformIgnorePatterns: [
-    'node_modules/(?!(remote-storage)/)'
-  ]
-};
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.js$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    },
+    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+    testPathIgnorePatterns: [
+        '/tests-e2e/',
+        '/tests/journeys/',
+        '/playwright-report/',
+        '/test-results/'
+    ],
+    collectCoverageFrom: [
+        'js/**/*.js',
+        '!js/app.js', // Skip app.js as it's mostly DOM manipulation
+        '!**/node_modules/**'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 70,
+            lines: 70,
+            statements: 70
+        }
+    },
+    verbose: true,
+    testTimeout: 10000,
+    transformIgnorePatterns: ['node_modules/(?!(remote-storage)/)']
+}
